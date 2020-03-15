21dd2a470ebba6d2e30fb8f0e3590993645a652b
Samuel Grayson <sam@samgrayson.me>
2020-03-15 01:24:47 -0500

    Compile open VINS on our system with Spindle/Switchboard integration

--- ov_msckf/src/slam2.cc
+++ ov_msckf/src/slam2.cc
@@ -16,25 +16,39 @@
 
 using namespace ILLIXR;
 
+using namespace ov_msckf;
+
 class slam1 : public component {
 public:
 	/* Provide handles to slam1 */
 	slam1(std::unique_ptr<writer<pose_type>>&& pose)
 		: _m_pose{std::move(pose)}
 		, _m_begin{std::chrono::system_clock::now()}
-	{
-		open_vins_estimator = new VioManager();
-	}
+	{ }
 
 	void feed_cam(const cam_type* cam_frame) {
+		const std::lock_guard<std::mutex> lock{_m_mutex};
 		// okvis_estimator.addImage(cvtTime(cam_frame->time), cam_frame->id, *cam_frame->img);
-		open_vins_estimator->feed_measurement_stereo(time_buffer, img0_buffer, img1_buffer, 0, 1);
+		assert(cam_frame->img0);
+		assert(cam_frame->img1);
+		cv::Mat img0 {*cam_frame->img0};
+		cv::Mat img1 {*cam_frame->img1};
+		open_vins_estimator.feed_measurement_stereo(cvtTime(cam_frame->time), img0, img1, 0, 1);
 		State* state = open_vins_estimator.get_state();
-		// TODO: unpack state 
+
+		auto quat = state->imu()->quat();
+
+		_m_pose->put(new pose_type{
+			state->imu()->pos(),
+				Eigen::Quaterniond{quat(3), quat(0), quat(1), quat(2)},
+			state->imu()->Rot(),
+		});
+
 	}
 
 	void feed_imu(const imu_type* imu_reading) {
-		open_vins_estimator->feed_measurement_imu(timem/1000000000.0, imu_reading->angular_v, imu_reading->linear_a);
+		const std::lock_guard<std::mutex> lock{_m_mutex};
+		open_vins_estimator.feed_measurement_imu(cvtTime(imu_reading->time), imu_reading->angular_v, imu_reading->linear_a);
 	}
 
 	virtual void _p_start() override {
@@ -48,15 +62,13 @@ public:
 private:
 	std::unique_ptr<writer<pose_type>> _m_pose;
 	time_type _m_begin;
+	std::mutex _m_mutex;
 
 	VioManager open_vins_estimator;
 	
-	okvis::Time cvtTime(time_type t) {
+	double cvtTime(time_type t) {
 		auto diff = t - _m_begin;
-		auto nanosecs = static_cast<uint64_t>(std::chrono::duration_cast<std::chrono::nanoseconds>(diff).count());
-		auto secs = static_cast<uint32_t>(nanosecs / 1000000);
-		auto nanosecs_rem = static_cast<uint32_t>(nanosecs - secs * 1000000);
-		return {secs, nanosecs_rem};
+		return static_cast<double>(std::chrono::duration_cast<std::chrono::nanoseconds>(diff).count()) / 1000000000.0;
 	}
 
 };
@@ -68,8 +80,7 @@ extern "C" component* create_component(switchboard* sb) {
 
 	auto this_slam1 = new slam1{std::move(pose_ev)};
 
-	sb->schedule<cam_type>("cam0", std::bind(&slam1::feed_cam, this_slam1, std::placeholders::_1));
-	sb->schedule<cam_type>("cam1", std::bind(&slam1::feed_cam, this_slam1, std::placeholders::_1));
+	sb->schedule<cam_type>("cams", std::bind(&slam1::feed_cam, this_slam1, std::placeholders::_1));
 	sb->schedule<imu_type>("imu0", std::bind(&slam1::feed_imu, this_slam1, std::placeholders::_1));
 
 	return this_slam1;
