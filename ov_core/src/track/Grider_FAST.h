236d5c5fe41070d752814b04855951919783a6f1
Samuel Grayson <sam@samgrayson.me>
2020-06-17 19:05:10 -0500

    Fixed print label

--- ov_core/src/track/Grider_FAST.h
+++ ov_core/src/track/Grider_FAST.h
@@ -94,7 +94,7 @@ namespace ov_core {
             int ct_rows = std::floor(img.rows/size_y);
             std::vector<std::vector<cv::KeyPoint>> collection(ct_cols*ct_rows);
             parallel_for_(cv::Range(0, ct_cols*ct_rows), [&](const cv::Range& range) {
-				PRINT_CPU_TIME_FOR_THIS_BLOCK("open_vins");
+				PRINT_CPU_TIME_FOR_THIS_BLOCK("open_vins_griding");
 
                 for (int r = range.start; r < range.end; r++) {
 
--- ov_msckf/src/core/VioManager.cpp
+++ ov_msckf/src/core/VioManager.cpp
@@ -175,8 +175,8 @@ void VioManager::feed_measurement_stereo(double timestamp, cv::Mat& img0, cv::Ma
     if(params.use_stereo) {
         trackFEATS->feed_stereo(timestamp, img0, img1, cam_id0, cam_id1);
     } else {
-        std::thread t_l = timed_thread("open_vins", &TrackBase::feed_monocular, trackFEATS, boost::ref(timestamp), boost::ref(img0), boost::ref(cam_id0));
-        std::thread t_r = timed_thread("open_vins", &TrackBase::feed_monocular, trackFEATS, boost::ref(timestamp), boost::ref(img1), boost::ref(cam_id1));
+        std::thread t_l = timed_thread("open_vins_feed_monocular", &TrackBase::feed_monocular, trackFEATS, boost::ref(timestamp), boost::ref(img0), boost::ref(cam_id0));
+        std::thread t_r = timed_thread("open_vins_feed_monocular", &TrackBase::feed_monocular, trackFEATS, boost::ref(timestamp), boost::ref(img1), boost::ref(cam_id1));
         t_l.join();
         t_r.join();
     }
