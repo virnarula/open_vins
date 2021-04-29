7ba993cecdfba10019d4a6b0d1b2a3871d5c9b0d
Vir Narula <virnarula@outlook.com>
2021-04-29 12:46:38 -0500

    add hpvm.h file

--- ov_core/src/track/hpvm.h
+++ ov_core/src/track/hpvm.h
@@ -1,5 +1,7 @@
 #pragma once
 
+extern void* __hpvm_launch_begin(unsigned, ...) noexcept;
+extern void __hpvm_launch_end(void*) noexcept;
 extern void* __hpvm__launch(void*, ...) noexcept;
 extern void __hpvm__wait(void*) noexcept;
 extern void* __hpvm_parallel_section_begin() noexcept;
